069f410ba53c5faca23a10860f266460471c5f1d
ZAKARIA-MOUFID <zakariamoufid766@gmail.com>
2024-08-07 18:21:43 +0100

    modified,

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
 
 int main() {
-    printf("to day we will learn git hub");
+    int C ;
+   float F ;
+   printf("veuillez saiser la température en Fahrenheit : ");
+   scanf("%f",&F);
+
+   C = (F-32)/1.8 ;
+
+   if ( C<=0 ) {
+    printf("%d : tres froid ",C);
+   }
+   else if ( C>=0 && C<=15 ){
+    printf("%d : froid",C);
+   }
+   else if ( C>=15 && C<=30){
+    printf("%d : chaud",C);
+   }
+   else {
+    printf("%d : tres chaud",C);
+   }
 
     return 0 ;
 }
